89d66f86310914e8fb7216263ff134375dd6e0cc
chandrakanth <chandrakanth@LENOVO-PC>
2018-10-05 15:30:25 +0530

    changed

--- angular 4 proj/ClientApp/app/app.server.module.ts
+++ angular 4 proj/ClientApp/app/app.server.module.ts
@@ -3,10 +3,12 @@ import { ServerModule } from '@angular/platform-server';
 import { AppModuleShared } from './app.shared.module';
 import { AppComponent } from './components/app/app.component';
 import { employeeComponent } from './components/app/Employee/employee.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     bootstrap: [ AppComponent ],
     imports: [
+        FormsModule,
         ServerModule,
         AppModuleShared
     ]
